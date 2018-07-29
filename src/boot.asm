6f4a29afab985999c54d21b4ff9566afd34bf1ac
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2018-07-29 12:58:26 +0800

    1K stack added

--- src/boot.asm
+++ src/boot.asm
@@ -1,22 +1,33 @@
-BITS 16    ; Tell NASM this is 16 bit code
-ORG 0x7C00 ; Tell NASM to output at offset 0x7C00
+BITS 16    ; 16-bit real-mode instructions
+ORG 0x7C00 ; Expect the bootloader to be loaded into memory at 0x7C00
 
-BOOT:
-    MOV SI, HELLO ; Point SI register to hello label memory location
-    MOV AH, 0x0E  ; 0x0E means write character in TTY mode
+STACK:
+	; Set up a 1K stack after this bootloader
+    	MOV AX, 0x7C00 ; Set AX equal to the location of xBoot
+    	ADD AX, 0x20   ; Skip over the size of the bootloader divided by 16
+	MOV SS, AX     ; Set Stack Segment (SS) to this location
+	MOV SP, 0x400  ; Set SS:SP at the top of our 1K stack
+
+BOOT:  
+	MOV SI, BOOTMSG ; Set the address of the null-terminated string message to the SI register
+	MOV AH, 0x0E    ; Output characters in TTY mode
 
 LOOP:
-    LODSB     ; Load byte at address DS:SI into AL
-    OR AL, AL ; Perform logical OR on the AL register
-    JZ HALT   ; If AL is zero, jump to HALT
-    INT 0x10  ; Runs BIOS interrupt 0x10 - video services
-    JMP LOOP  ; Jump and loop again
+	LODSB     ; Load byte at address DS:SI into AL
+	OR AL, AL ; Trigger Zero Flag (ZF) if result is zero
+	JZ HALT   ; Jump to HALT since ZF is set
+	INT 0x10  ; Run BIOS interrupt vector and print the character
+	JMP LOOP  ; Repeat for the next character
 
 HALT:
-    CLI ; Clear interrupt flag
-    HLT ; Halt execution
+	CLI ; Clear the interrupt flag
+	HLT ; Halt the execution
 
-HELLO: DB 0x0A, "Hello World! Booted from xBoot", 0
+BOOTMSG: DB "Hello, I am xBoot", 0 ; The message we want to display
 
-TIMES 510 - ($-$$) DB 0 ; Pad remaining 510 bytes with zeroes
-DW 0xAA55               ; Magic bootloader magic - marks this 512 byte sector bootable!
+TIMES 510 - ($-$$) DB 0 ; Pad the remaining 510-bytes with zeros
+DW 0xAA55               ; Boot signature
+	
+	
+	
+	
