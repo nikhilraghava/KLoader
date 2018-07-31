7b99a47e7f07a444b7457caeca66aa53fc621af8
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2018-07-31 10:03:16 +0800

    subroutine to display text added

--- src/boot.asm
+++ src/boot.asm
@@ -1,24 +1,32 @@
 BITS 16    ; 16-bit real-mode instructions
 ORG 7C00H  ; Expect the bootloader to be loaded into memory at 0x7C00
 
-STACK:  MOV AX, 7C0H  ; Set AX equal to the location of xBoot
-    	ADD AX, 20H   ; Skip over the size of the bootloader divided by 16
-	MOV SS, AX    ; Set Stack Segment (SS) to this location
-	MOV SP, 400H  ; Set SS:SP at the top of our 1K stack
+STACK: MOV AX, 7C0H  ; Set AX equal to the location of xBoot
+       ADD AX, 20H   ; Skip over the size of the bootloader divided by 16
+       MOV SS, AX    ; Set Stack Segment (SS) to this location
+       MOV SP, 400H  ; Set SS:SP at the top of our 1K stack
 
-BOOT:	MOV SI, BOOTMSG ; Set the address of the null-terminated string message to the SI register
-	MOV AH, 0EH     ; Output characters in TTY mode
+BOOT: MOV SI, BOOTMSG ; Set the address of the null-terminated string message to the SI register
+      CALL DISP       ; Call the display subroutine
+      MOV SI, SCNDMSG ; Set the address of the null-terminated string message to the SI register
+      CALL DISP       ; Call the display subroutine
+      CALL HALT       ; Halt execution
 
-LOOP:	LODSB     ; Load byte at address DS:SI into AL and increment SI
-	OR AL, AL ; Trigger Zero Flag (ZF) if result is zero
-	JZ HALT   ; Jump to HALT if ZF is set
-	INT 10H   ; Run BIOS interrupt vector and print the character
-	JMP LOOP  ; Repeat for the next character
+DISP: MOV AH, 0EH ; Output characters in TTY mode
 
-HALT:	CLI ; Clear the interrupt flag
-	HLT ; Halt the execution
+.DCHAR: LODSB      ; Load byte at address DS:SI into AL and increment SI
+	OR AL, AL  ; Trigger Zero Flag (ZF) if result is zero
+	JZ .DONE   ; Jump to HALT if ZF is set
+	INT 10H    ; Run BIOS interrupt vector and print the character
+	JMP .DCHAR ; Repeat for the next character
 
-BOOTMSG: DB 0AH, "Hello, I am xBoot", 0 ; Display the message on a new line
+.DONE: RET ; Return 
+
+HALT: CLI ; Clear the interrupt flag
+      HLT ; Halt the execution
+
+BOOTMSG: DB 0AH, "Hello, I am xBoot. A minimalistic x86 bootloader.", 0 ; Display the message on a new line
+SCNDMSG: DB 0AH, 0DH, "Written by Nikhil Raghavendra", 0                ; Display the message on a new line with CR
 
 TIMES 510 - ($-$$) DB 0 ; Pad the remaining 510-bytes with zeros
-DW 0xAA55               ; Boot signature
+DW 0xAA55               ; Boot signature in little-endian order
